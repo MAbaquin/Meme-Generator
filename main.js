17623d752cc99cecf4caccc735b1ba11bc7d35d0
Miguel Abaquin <miguelabaquin@gmail.com>
2020-08-17 12:54:10 +0100

    adding dotenv

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const discord = require('discord.js');
 const client = new discord.Client();
 const handler = require('./commandHandler.js');
@@ -6,6 +7,7 @@ const prefix = '.mmg';
 client.commands = new discord.Collection();
 
 handler.init(client);
+client.on('ready', () => client.user.setActivity('The Dankest of Memes', { type: WATCHING }));
 client.on('message', message => handler.parse(prefix, message));
 
-client.login('NzQ0NTQzNDczNjMzMzI5MTky.XzkwJQ.w8-FnRVxRPwtikAaniMSz2ehQUk');
+client.login(process.env.TOKEN);
